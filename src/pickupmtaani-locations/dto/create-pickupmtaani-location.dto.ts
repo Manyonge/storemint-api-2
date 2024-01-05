6ce580c0c11abecc275a3acbada294f1b31abfe8
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-01-05 15:15:44 +0300

    testing pickup mtaani service

--- src/pickupmtaani-locations/dto/create-pickupmtaani-location.dto.ts
+++ src/pickupmtaani-locations/dto/create-pickupmtaani-location.dto.ts
@@ -1,1 +1,7 @@
-export class CreatePickupmtaaniLocationDto {}
+import { PickType } from "@nestjs/mapped-types";
+import { PickupmtaaniLocation } from "../entities/pickupmtaani-location.entity";
+
+export class CreatePickupmtaaniLocationDto extends PickType(
+  PickupmtaaniLocation,
+  ["location"] as const,
+) {}
