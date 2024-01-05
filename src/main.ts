63c28dc3ed0ef2b3aaca50b6238b219508728cca
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-01-05 16:52:03 +0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: "http://localhost:5173",
     credentials: true,
   });
   app.use(cookieParser());
