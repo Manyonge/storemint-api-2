28e96cca3c333ffde72c4521814a1d57958c7639
manyonge <arthurmanyonge22@gmail.com>
2024-03-10 06:35:25 +0000

    add_globa_prefix_api

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as dotenv from "dotenv";
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     credentials: true,
