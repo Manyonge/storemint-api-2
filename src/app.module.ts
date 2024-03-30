6fbf5d79118370af7b1957e746eec5d65be3f863
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-03-30 14:06:31 +0300

    set up validation for route param id

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,10 +15,10 @@ import { PickupmtaaniLocationsModule } from "./pickupmtaani-locations/pickupmtaa
 import { PickupmtaaniAgentsModule } from "./pickupmtaani-agents/pickupmtaani-agents.module";
 import { PrismaModule } from "nestjs-prisma";
 import { APP_PIPE } from "@nestjs/core";
-import { CheckRetailerPipe } from "./pipes/check-retailer.pipe";
 import { OrdersModule } from "./orders/orders.module";
 import { PaymentsModule } from "./payments/payments.module";
 import { LoggingMiddleware } from "./middleware/logging.middleware";
+import { AppPipe } from "./pipes/app.pipe";
 
 @Module({
   imports: [
@@ -45,7 +45,7 @@ import { LoggingMiddleware } from "./middleware/logging.middleware";
     AppService,
     {
       provide: APP_PIPE,
-      useClass: CheckRetailerPipe,
+      useClass: AppPipe,
     },
   ],
 })
---
+++ src/pipes/app.pipe.ts
@@ -0,0 +1,69 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from "@nestjs/common";
+import { PrismaService } from "nestjs-prisma";
+import { isWholeNumber } from "../helpers";
+
+@Injectable()
+export class AppPipe implements PipeTransform {
+  constructor(private readonly prisma: PrismaService) {}
+  async transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      if (!!value?.retailerId) {
+        return await this.checkRetailerId(value);
+      }
+      if (!!value?.productId) {
+        return await this.checkProductId(value);
+      }
+      if (
+        !!value?.id ||
+        (metadata?.type === "param" && metadata?.data === "id")
+      ) {
+        return await this.checkId(value);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async checkRetailerId(value: any) {
+    const retailerId = value.retailerId;
+    if (!isWholeNumber(retailerId)) {
+      throw new BadRequestException(
+        "retailerId must be a whole number greater than 0",
+      );
+    }
+    const retailer = await this.prisma.retailer.findUnique({
+      where: { id: Number(retailerId) },
+    });
+    if (!retailer) {
+      throw new BadRequestException("Retailer not found");
+    }
+    return value;
+  }
+  async checkProductId(value: any) {
+    const productId = value.productId;
+    if (!isWholeNumber(productId)) {
+      throw new BadRequestException(
+        "productId must be a whole number greater than 0",
+      );
+    }
+    const product = await this.prisma.storeProduct.findUnique({
+      where: { id: Number(productId) },
+    });
+    if (!product) {
+      throw new BadRequestException("Product not found");
+    }
+    return value;
+  }
+  async checkId(value: any) {
+    const id = value?.id;
+    if (!isWholeNumber(id)) {
+      throw new BadRequestException("id must be a whole number greater than 0");
+    }
+    return value;
+  }
+}
