a299f736ea07889251a5e1f023124aaea9351cc9
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-03-31 19:42:45 +0300

    (staff_endpoints) protect all staff end points

--- src/staff/staff.controller.ts
+++ src/staff/staff.controller.ts
@@ -6,31 +6,37 @@ import {
   Param,
   Post,
   Query,
+  UseGuards,
 } from "@nestjs/common";
 import { StaffService } from "./staff.service";
 import { CreateStaffDto } from "./dto/create-staff.dto";
 import { CheckIdParamPipe } from "../pipes/check-id-param-pipe.service";
 import { QueryParamDto } from "./dto/QueryParam.dto";
+import { AuthGuard } from "../auth/auth.guard";
 
 @Controller("staff")
 export class StaffController {
   constructor(private readonly staffService: StaffService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createStaffDto: CreateStaffDto) {
     return this.staffService.create(createStaffDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query() queryParamDto: QueryParamDto) {
     return this.staffService.findAll(+queryParamDto.retailerId);
   }
 
+  @UseGuards(AuthGuard)
   @Get(":id")
   findOne(@Param("id", new CheckIdParamPipe()) id: string) {
     return this.staffService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(":id")
   remove(@Param("id", new CheckIdParamPipe()) id: string) {
     return this.staffService.remove(+id);
