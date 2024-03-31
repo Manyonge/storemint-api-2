d463e457222483aff2d53a1ee89f1ae9d6e6a60e
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-03-31 22:12:03 +0300

    (dev) wildcard cors

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:6000",
-      "http://localhost:5174",
-      "https://dripventory.storemint.shop",
-      "https://storemint.shop",
-    ],
+    origin: ["localhost", "*.storemint.shop"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
   app.use(cookieParser());
