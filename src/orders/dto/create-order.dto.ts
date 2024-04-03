d56db58d75a0fd2ca723fc62f8f7ad58e45bb724
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-04-03 18:35:10 +0300

    (orders_resource) create create order dto

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,49 @@
+import { OrderTypeEnum } from "../../users/enums/OrderType.enum";
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+} from "class-validator";
+
+export class CreateOrderDto {
+  @IsNotEmpty()
+  @IsInt()
+  retailerId: number;
+  @IsNotEmpty()
+  @IsEnum(OrderTypeEnum)
+  type: OrderTypeEnum;
+  @IsNotEmpty()
+  @IsInt()
+  @Min(120, { message: "delivery fee must be at least Ksh 120" })
+  deliveryFee: number;
+  @IsNotEmpty()
+  @IsString()
+  clientName: string;
+  @IsNotEmpty()
+  @IsString()
+  clientPhone: string;
+  @IsOptional()
+  @IsString()
+  receiverLocation: string;
+  @IsOptional()
+  @IsString()
+  receiverAgent: string;
+  @IsOptional()
+  @IsString()
+  doorstepAddress: string;
+  @IsOptional()
+  @IsInt()
+  balance: number;
+  @IsOptional()
+  @IsString()
+  errandLocation: string;
+  @IsOptional()
+  @IsString()
+  errandSacco: string;
+  @IsOptional()
+  @IsString()
+  specialInstructions: string;
+}
--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,27 @@
-export class Order {}
+import { OrderTypeEnum } from "../../users/enums/OrderType.enum";
+
+export class Order {
+  id: number;
+
+  retailerId: number;
+
+  type: OrderTypeEnum;
+
+  deliveryFee: number;
+  clientName: string;
+  clientPhone: string;
+
+  receiverLocation: string;
+
+  receiverAgent: string;
+
+  doorstepAddress: string;
+
+  balance: number;
+
+  errandLocation: string;
+
+  errandSacco: string;
+
+  specialInstructions: string;
+}
