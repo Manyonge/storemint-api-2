092569508f5f66b269ce3a0ad4fef2dfeef36943
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-05-30 18:58:21 +0300

    update retailer services and controllers

--- src/retailers/dto/create-through-google.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
-
-export class CreateThroughGoogleDto {
-  @IsNotEmpty()
-  uid: number;
-  @IsNotEmpty()
-  @IsEmail()
-  businessEmail: string;
-}
